5b3974a35a7d879df9977d0be12f400c206b9454
Wessel van Ree <wesselvanree@gmail.com>
2020-07-08 15:56:08 +0200

    Updates

---
+++ precache-manifest.0d425f3525c1774a33f7f8ae38a956db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c106704944ded1355edf9ab3b28b615a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd11cff46ef38c110129",
+    "url": "/portfolio/static/css/main.d7724608.chunk.css"
+  },
+  {
+    "revision": "434baa9458c9e1570e62",
+    "url": "/portfolio/static/js/2.2f3e323a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.2f3e323a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd11cff46ef38c110129",
+    "url": "/portfolio/static/js/main.52e635e7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6de888c5513be086e26b6fedbd49eca9",
+    "url": "/portfolio/static/media/hydrautronics.6de888c5.jpg"
+  },
+  {
+    "revision": "96161cf610e40e51443d1e85c9db66c2",
+    "url": "/portfolio/static/media/ivdh.96161cf6.jpg"
+  }
+]);
