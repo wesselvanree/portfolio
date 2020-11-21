6f271b30d5fbdf2c693194180f26c835f0cc546f
Wessel van Ree <wesselvanree@gmail.com>
2020-11-21 19:40:43 +0100

    404 Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,31 @@ import Navigation from "./Navigation";
 import Home from "./Pages/Home";
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <Navigation />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Redirect to="/"></Redirect>
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+class App extends React.Component {
+  // Wanneer deze pagina ingeladen wordt door 404.html wordt het vraagteken verwijderd
+  removeQuestionMark() {
+    let search = document.location.search.replace("?/", "");
+    console.log(search);
+    window.history.replaceState('', '', search);
+  }
+
+  render() {
+    return (
+      <>
+        <Navigation />
+        <BrowserRouter>
+          <Switch>
+            {/* <Route path="/?/" component={Navigation} /> */}
+            <Route exact path="/" render={() => {
+              this.removeQuestionMark();
+              return <Home />
+            }} />
+            <Redirect to="/"/>
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
