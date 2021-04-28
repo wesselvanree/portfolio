d302f62f1a8a5f23b36b2c6094eb93ebb9959d7b
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2021-04-28 17:45:13 +0200

    add key prop to list items

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './footer.scss';
 import ExternalLink from './ExternalLink';
 import { socials } from './data';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
   const socialsList = socials
     .sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
-    .map(({ name, url, external }) => (
-      <li>
+    .map(({ name, url, external }, i) => (
+      <li key={i}>
         {external ? (
           <ExternalLink text={name} url={url} className="grey" />
         ) : (
@@ -25,18 +27,25 @@ const Footer = () => {
       <div className="footer--top">
         <div className="footer--left">
           <h3>Wessel van Ree</h3>
-          <p>
-            Student Econometrics and Operational Research at Erasmus University
+          <p className="footer-paragraph">
+            Econometrics and Operational Research Student at Erasmus University
             Rotterdam.
           </p>
+          <div
+            style={{ marginTop: '30px', marginBottom: 0, paddingLeft: '10px' }}
+            className="horizontal-flex footer-grey"
+          >
+            <FontAwesomeIcon style={{ opacity: 0.8 }} icon={faMapMarkerAlt} />
+            <p style={{ margin: '0 0 0 10px' }}>Netherlands</p>
+          </div>
         </div>
-        <div className="footer--right">
+        <div className="footer-column footer--right">
           <h4>Social Links</h4>
           <ul>{socialsList}</ul>
         </div>
       </div>
       <div className="footer--bottom">
-        <p>&copy; {currentYear} Wessel van Ree</p>
+        <p>Copyright &copy; {currentYear} Wessel van Ree</p>
       </div>
     </footer>
   );
